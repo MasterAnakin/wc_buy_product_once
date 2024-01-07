9e582014c0fd70f47dd859f964dcaaa99f0f1d58
MasterAnakin <mbm4816@gmail.com>
2024-01-07 13:26:43 +0100

    Update wc_product_check.php

--- wc_product_check.php
+++ wc_product_check.php
@@ -1,19 +1,21 @@
 <?php
-
+/**
+ * Allow user to buy specific product only once.
+ */
 function filter_woocommerce_add_to_cart_validation( $passed, $product_id, $quantity, $variation_id = null, $variations = null ) {
-    // Retrieve the current user object
-    $current_user = wp_get_current_user();
-    
-	if ( $product_id === add_your_id ){
-		// Checks if a user (by email or ID or both) has bought an item
+	// Retrieve the current user object.
+	$current_user = wp_get_current_user();
+
+	if ( add_your_id === $product_id ) {
+		// Checks if a user (by email or ID or both) has bought an item.
 		if ( wc_customer_bought_product( $current_user->user_email, $current_user->ID, add_your_id ) ) {
-			// Display an error message
+			// Display an error message.
 			wc_add_notice( __( 'You can buy this ticket only once.', 'woocommerce' ), 'error' );
 
 			$passed = false;
 		}
 	}
 
-    return $passed;
+	return $passed;
 }
 add_filter( 'woocommerce_add_to_cart_validation', 'filter_woocommerce_add_to_cart_validation', 10, 5 );
